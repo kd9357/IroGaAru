0115c40c29279179c2b98f99be67efce23d00dc9
Persia Ghaffari <persiaghaffari@gmai.com>
2017-09-05 14:23:59 -0500

    Added Unity Project
    Made demo scene and started on player physics

---
+++ Omnis/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float Speed;
+    public float Jump;
+
+    private Rigidbody2D _rb;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    float x_mov = Input.GetAxis("Horizontal") * Speed;
+	    float y_mov = Input.GetKeyUp(KeyCode.Space) ? Jump : 0;
+
+	    _rb.velocity = new Vector2(x_mov, y_mov);
+	}
+}
