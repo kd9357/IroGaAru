586cb08bf04f2a5cfa86f7e1ecb0d41c7f34e72c
Persia Ghaffari <persiaghaffari@gmai.com>
2017-09-13 17:28:51 -0500

    Made v1 build

--- Omnis/Assets/Scripts/Player.cs
+++ Omnis/Assets/Scripts/Player.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class Player : MonoBehaviour {
 
