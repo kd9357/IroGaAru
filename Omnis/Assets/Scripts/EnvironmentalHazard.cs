1ef55697402f786edfb0473ca92462e7804dca5e
Persia Ghaffari <persiaghaffari@gmai.com>
2017-09-21 15:22:58 -0500

    Added environmental spikes

---
+++ Omnis/Assets/Scripts/EnvironmentalHazard.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentalHazard : MonoBehaviour {
+
+    [Tooltip("How much damage the hazard deals on contact")]
+    public int TouchDamage = 1;
+
+    // Audio vars
+    public AudioClip[] HazardSoundEffects;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    #region Collisions
+    // Hurt player on contact
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.collider.tag)
+        {
+            case "Player":
+                var player = collision.gameObject.GetComponent<Player>();
+                if (player.IsInvincible())
+                    return;
+
+                player.PlayerDamaged(TouchDamage);
+                player.Knockback(collision.transform.position.x < transform.position.x);
+
+                // Enemy hit sound
+                _audioSource.clip = HazardSoundEffects[0];
+                _audioSource.Play();
+                break;
+        }
+    }
+
+    //This is necessary if the player is pushing against the enemy while invincible, and their invincibility wears off
+    protected virtual void OnCollisionStay2D(Collision2D collision)
+    {
+        switch (collision.collider.tag)
+        {
+            case "Player":
+                var player = collision.gameObject.GetComponent<Player>();
+                if (player.IsInvincible())
+                    return;
+
+                player.PlayerDamaged(TouchDamage);
+                player.Knockback(collision.transform.position.x < transform.position.x);
+
+                // Enemy hit sound
+                _audioSource.clip = HazardSoundEffects[0];
+                _audioSource.Play();
+                break;
+        }
+    }
+
+    #endregion
+}
