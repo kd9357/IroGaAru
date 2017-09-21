dc7705552e710a5ad93cf1b764c498751a33f2d5
Kevin Du <kevin_w_du@utexas.edu>
2017-09-20 21:33:45 -0500

    Added CameraFollow script + increased the size of the camera from 5 to 8.

---
+++ Omnis/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Taken from https://www.youtube.com/watch?time_continue=1&v=4qE8cuHI93c time 10:25
+
+public class CameraFollow : MonoBehaviour {
+
+    [Tooltip("Distance in the x axis the player can move before the camera follows")]
+    public float XMargin = 1f;
+    [Tooltip("Distance in the y axis the player can move before the camera follows")]
+    public float YMargin = 1f;
+    [Tooltip("How smoothly the camera catches up with it's target movement in the x axis (larger values = less smooth)")]
+    public float XSmooth = 8f;
+    [Tooltip("How smoothly the camera catches up with it's target movement in the y axis (larger values = less smooth)")]
+    public float YSmooth = 8f;
+    [Tooltip("Check if Camera has minimum and maximum range")]
+    public bool CameraBounded = false;
+    [Tooltip("The minimum x and y coordinates the camera can have")]
+    public Vector2 MinXY;
+    [Tooltip("The maximum x and y coordinates the camera can have")]
+    public Vector2 MaxXY;
+
+    private Transform _player;
+
+    void Awake () {
+        _player = GameObject.FindGameObjectWithTag("Player").transform; 
+	}
+	
+    bool CheckXMargin()
+    {
+        return Mathf.Abs(transform.position.x - _player.position.x) > XMargin;
+    }
+
+    bool CheckYMargin()
+    {
+        return Mathf.Abs(transform.position.y - _player.position.y) > YMargin;
+    }
+
+    void FixedUpdate () {
+        TrackPlayer();
+	}
+
+    void TrackPlayer()
+    {
+        // By default the target x and y coordinates of the camera are it's current x and y coordinates
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        //Check if outside deadzone
+        if (CheckXMargin())
+            targetX = Mathf.Lerp(transform.position.x, _player.position.x, XSmooth * Time.deltaTime);
+        if (CheckYMargin())
+            targetY = Mathf.Lerp(transform.position.y, _player.position.y, YSmooth * Time.deltaTime);
+
+        //Clamp to Min & MaxXY
+        if(CameraBounded)
+        {
+            targetX = Mathf.Clamp(targetX, MinXY.x, MaxXY.x);
+            targetY = Mathf.Clamp(targetY, MinXY.y, MaxXY.y);
+        }
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
---
+++ Omnis/Assets/Scripts/SmoothCamera2D.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SmoothCamera2D : MonoBehaviour {
+
+    public float SmoothTimeX;
+    public float SmoothTimeY;
+    public Transform Target;
+    [Tooltip("Horizontal offset camera centers on")]
+    public float XOffset = 0;
+    [Tooltip("Vertical offset camera centers on")]
+    public float YOffset = 0;
+
+    public bool CameraBounds;
+
+    [Tooltip("The minimum x, y and z the camera may go to in world coordinates")]
+    public Vector3 MinCameraPos;
+    [Tooltip("The maximum x, y and z the camera may go to in world coordinates")]
+    public Vector3 MaxCameraPos;
+
+    private Camera _camera;
+    private Vector2 _velocity;
+
+    void Start()
+    {
+        _camera = GetComponent<Camera>();    
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 center = Target.position + new Vector3(XOffset, YOffset, 0);
+        float posX = Mathf.SmoothDamp(_camera.transform.position.x, center.x, ref _velocity.x, SmoothTimeX);
+        float posY = Mathf.SmoothDamp(_camera.transform.position.y, center.y, ref _velocity.y, SmoothTimeY);
+
+        _camera.transform.position = new Vector3(posX, posY, _camera.transform.position.z);
+
+        if(CameraBounds)
+        {
+            _camera.transform.position = new Vector3(Mathf.Clamp(_camera.transform.position.x, MinCameraPos.x, MaxCameraPos.x),
+                Mathf.Clamp(_camera.transform.position.x, MinCameraPos.x, MaxCameraPos.x),
+                Mathf.Clamp(_camera.transform.position.z, MinCameraPos.z, MaxCameraPos.z));
+
+        }
+    }
+
+
+
+}
