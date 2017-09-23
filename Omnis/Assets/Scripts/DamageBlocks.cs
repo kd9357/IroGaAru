8e3025710b77ad92979d931e23cfc64f81152616
Jtr <jtr@utexas.edu>
2017-09-23 16:21:39 -0500

    UI Updated from feedback

---
+++ Omnis/Assets/Scripts/DamageBlocks.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageBlocks : MonoBehaviour {
+
+    public bool is_moving = false;
+    public float gravity;
+    public float velocity_x;
+    public float velocity_y;
+    public float speed;
+    public float timeout;
+    private float throw_timer = 0.0f;
+    private RectTransform tf;
+    private Vector3 init_position;
+    private Quaternion init_rotation;
+	// Use this for initialization
+	void Start () {
+        tf = GetComponent<RectTransform>();
+        init_position = tf.localPosition;
+        init_rotation = tf.localRotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (is_moving == true)
+        {
+            Vector3 v = new Vector3(velocity_x, velocity_y, 0);
+            Vector3 g = new Vector3(0, gravity, 0);
+            tf.localPosition = tf.localPosition + v* throw_timer + 0.5f * g * throw_timer* throw_timer;
+            throw_timer += speed;
+            tf.Rotate(Vector3.back * throw_timer);
+            if(throw_timer > timeout)
+            {
+                is_moving = false;
+                throw_timer = 0.0f;
+                tf.localPosition = init_position;
+                tf.localRotation = init_rotation;
+                this.transform.gameObject.SetActive(false);
+            }
+        }
+	}
+
+    public void Fall() //take damage
+    {
+        throw_timer = 1;
+        is_moving = true;
+    }
+
+    public void Reset()
+    {
+        is_moving = false;
+        throw_timer = 0.0f;
+        tf.localPosition = init_position;
+        tf.localRotation = init_rotation;
+    }
+}
--- Omnis/Assets/Scripts/HPDisplay.cs
+++ Omnis/Assets/Scripts/HPDisplay.cs
@@ -10,17 +10,40 @@ public class HPDisplay : MonoBehaviour {
     public Image[] hp_array;
     public int danger_number;
 
+    private int previous_hp;
+
     void Start () {
         GUIElement[] hp_array = new GUIElement[player.MaxHealth];
-	}
+        previous_hp = 8;
+    }
 	
 	// OnGUI called to draw GUI objects.
 	void OnGUI () {
 	    int player_health = player.GetCurrentHealth();
+        if(player_health < previous_hp)
+        {
+            
+            for (int i = previous_hp; i > player_health; i--)
+            {
+                
+                DamageBlocks a = hp_array[i-1].GetComponent<DamageBlocks>();
+                a.Fall();
+            }
+        }
+        else if (player_health > previous_hp)
+        {
+            for (int i = previous_hp; i < player_health; i++)
+            {
+                print(i);
+                DamageBlocks a = hp_array[i].GetComponent<DamageBlocks>();
+                a.Reset();
+                hp_array[i].transform.gameObject.SetActive(true);
+            }
+        }
+        previous_hp = player_health;
         for (int i = 0; i < player.MaxHealth; i++) {
             if (i < player_health)
             {
-                hp_array[i].enabled = true;
                 if (player_health <= danger_number)
                 {
                     hp_array[i].color = new Color(1,0,0);
@@ -30,11 +53,8 @@ public class HPDisplay : MonoBehaviour {
                     hp_array[i].color = new Color(1,1,1);
                 }
             }
-            else
-            {
-                hp_array[i].enabled = false;
-            }
             
 		}
-	}
+    }
+
 }
