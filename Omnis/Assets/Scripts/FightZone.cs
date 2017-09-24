88de5315b965f0ae83ffa73e231777d96090869e
Kevin Du <kevin_w_du@utexas.edu>
2017-09-24 00:32:28 -0500

    Rudimentary fight zone added (needs a lot of improvement)
    Current limitations:
    -There must be an enemy already existing in the game for the zone to function properly.
    -The game will spawn only one enemy after initial enemies are destroyed

--- Omnis/Assets/Scripts/FightZone.cs
+++ Omnis/Assets/Scripts/FightZone.cs
@@ -4,41 +4,66 @@ using UnityEngine;
 
 public class FightZone : MonoBehaviour {
     
+    //TODO: just use a separate spawner script
+
     //If in the future different criteria other than destroy all used, must update
     [Tooltip("The list of enemies or objects to be destroyed before moving on")]
-    public GameObject[] Challenges;
+    public List<GameObject> Challenges;
+    [Tooltip("The list enemy or object that will spawn after a wave")]
+    public GameObject ReserveChallenge;
+    [Tooltip("The total number of waves of enemies to be destroyed")]
+    public int Waves = 1;
+
     private Camera _camera;
     private Transform _target;
     private Transform _player;
+    private Transform _spawnPoint;
 
-    //testing as public first
-    public bool _unlock = false;
+    private int _currentWave = 0;
 
 	// Use this for initialization
 	void Start () {
         _camera = Camera.main;
-        _target = gameObject.transform.GetChild(0).transform;
+        _target = transform.Find("Camera Center");
         _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _spawnPoint = transform.Find("Spawn Point");
     }
 	
 	// Update is called once per frame
 	void Update () {
-        //TODO: find a way to restrict Player movement while locked on
-        //suggestions: activate box colliders around camera frustum
-        //             check if player position outside frustum -> clamp position
+        if (WaveCompleted())
+        {
+            //Move on to next wave
+            if(_currentWave < Waves)
+            {
+                //Clear out list
+                Challenges.Clear();
+                //Spawn more enemies
+                Challenges.Add(Instantiate(ReserveChallenge, _spawnPoint.position, Quaternion.identity));
+                Debug.Log("Added challenge, count: " + Challenges.Count);
+                _currentWave++;
+            }
+            else
+                UnlockZone();
+        }
+            
+	}
 
-        _unlock = true;
+    void UnlockZone()
+    {
+        _camera.GetComponent<CameraFollow>().SetTarget(_player, true);
+        Destroy(gameObject);
+    }
+
+    bool WaveCompleted()
+    {
         foreach(GameObject go in Challenges)
         {
-            if(go != null)
-            {
-                _unlock = false;
-                break;
-            }
+            if (go != null)
+                return false;
         }
-        if (_unlock)
-            _camera.GetComponent<CameraFollow>().SetTarget(_player, true);
-	}
+        return true;
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
