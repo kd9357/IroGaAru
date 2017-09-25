8d29b4f4ba46355c3e4961c78a0a05127df6505e
Kevin Du <kevin_w_du@utexas.edu>
2017-09-24 20:42:11 -0500

    Fight zone improved
    Can now specify specific enemies to spawn for each wave, their location, and order

--- Omnis/Assets/Scripts/FightZone.cs
+++ Omnis/Assets/Scripts/FightZone.cs
@@ -6,19 +6,26 @@ using UnityEngine;
 
 public class FightZone : MonoBehaviour {
 
-    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+    public enum SpawnState { SPAWNING, WAITING, COUNTING, INACTIVE };
 
     [System.Serializable]
     public class Wave
     {
         [Tooltip("The identifier for this wave")]
         public string Name;
-        [Tooltip("The challenge to be spawned in this wave")]
-        public GameObject Challenge;
-        [Tooltip("The number of the above challenges to be spawned in this wave")]
-        public int Count;
-        [Tooltip("The rate at which a new challenge will be spawned, until Count has been reached")]
-        public float SpawnRate;
+        [Tooltip("The list of challenges to be spawned in this wave")]
+        public SpawnDetails[] NewChallenges;
+        [Tooltip("The amount of time before the next NewChallenge is spawned")]
+        public float Delay = 1f;
+    }
+
+    [System.Serializable]
+    public class SpawnDetails
+    {
+        [Tooltip("Drag a prefab for the folder here")]
+        public GameObject SpawnObject;
+        [Tooltip("Drag an existing spawn point here")]
+        public Transform SpawnPoint;
     }
 
     [Tooltip("Each unique round that occurs after initial challenges destroyed")]
@@ -29,15 +36,13 @@ public class FightZone : MonoBehaviour {
     //If in the future different criteria other than destroy all is used, must update
     [Tooltip("The initial challenges that must be destroyed before the next wave triggers (If empty starts wave 0 immediately)")]
     public List<GameObject> ChallengeList;
-    [Tooltip("The list of spawn points enemies will randomly spawn out of (If empty enemies will spawn from fightzone position)")]
-    public Transform[] SpawnPoints;
 
     private Transform _target;
     private Transform _player;
 
     private int _nextWave = -1;
     private float _waveCountdown;
-    private SpawnState _state = SpawnState.WAITING;
+    private SpawnState _state = SpawnState.INACTIVE;
 
     // Use this for initialization
     void Start () {
@@ -47,11 +52,16 @@ public class FightZone : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        if (_state == SpawnState.INACTIVE)
+            return;
         if (_state == SpawnState.WAITING)
         {
             //Check if enemies are dead
             if (WaveCompleted())
+            {
                 StartNewWave();
+                return;
+            }
             else
                 return;
         }
@@ -65,6 +75,7 @@ public class FightZone : MonoBehaviour {
             _waveCountdown -= Time.deltaTime;
     }
 
+    //Check if all waves completed, otherwise clear list out and increment wave number
     void StartNewWave()
     {
         if(_nextWave + 1 > Waves.Length - 1)
@@ -78,6 +89,7 @@ public class FightZone : MonoBehaviour {
         }
     }
 
+    //Check the list of challenges, if any are not null (still active) false
     bool WaveCompleted()
     {
         foreach (GameObject challenge in ChallengeList)
@@ -88,29 +100,25 @@ public class FightZone : MonoBehaviour {
         return true;
     }
 
-    //Begin wave, spawning a challenge at a defined SpawnRate
+    //Begin wave, spawning its NewChallenges
     IEnumerator SpawnWave(Wave wave)
     {
         _state = SpawnState.SPAWNING;
-        for(int i = 0; i < wave.Count; i++)
+        for(int i = 0; i < wave.NewChallenges.Length; i++)
         {
-            SpawnChallenge(wave.Challenge);
-            //Add to challenge list
-            yield return new WaitForSeconds(1f / wave.SpawnRate);
+            SpawnChallenge(wave.NewChallenges[i]);
+            yield return new WaitForSeconds(wave.Delay);
         }
         _state = SpawnState.WAITING;
         yield break;
     }
 
-    //Instantiate a challenge game object either at a spawn point or default position
-    void SpawnChallenge(GameObject challenge)
+    //Create the object given the SpawnDetails, defaulting to the center of the fightzone transform
+    void SpawnChallenge(SpawnDetails nextObject)
     {
         //Default to center of trigger fight zone
-        Transform spawnPos = transform;
-        //If we have spawn points set up, randomly choose one of them
-        if(SpawnPoints.Length > 0)
-            spawnPos = SpawnPoints[Random.Range(0, SpawnPoints.Length)];
-        ChallengeList.Add(Instantiate(challenge, spawnPos.position, spawnPos.rotation));
+        Transform spawnPosition = nextObject.SpawnPoint != null ? nextObject.SpawnPoint : transform;
+        ChallengeList.Add(Instantiate(nextObject.SpawnObject, spawnPosition.position, spawnPosition.rotation));
     }
 
     //All waves completed, unlock camera and destroy self
@@ -120,18 +128,16 @@ public class FightZone : MonoBehaviour {
         Destroy(gameObject);
     }
 
-    #region Collisions
-
     //When player enters zone, lock camera and begin wave process
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player"))
         {
             Camera.main.GetComponent<CameraFollow>().SetTarget(_target, false);
+            _state = SpawnState.WAITING;
             if(_waveCountdown <= 0)
                 _waveCountdown = TimeBetweenWaves;
         }
     }
 
-    #endregion
 }
