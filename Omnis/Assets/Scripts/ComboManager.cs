beead5aeaa7476ede3e9a520d9cf54050ba841dd
Persia <persia@cs.utexas.edu>
2017-11-02 11:40:32 -0500

    Added shaking/color lerp animation for combo

--- Omnis/Assets/Scripts/ComboManager.cs
+++ Omnis/Assets/Scripts/ComboManager.cs
@@ -4,8 +4,7 @@
  * Include Files
  */
 
-using System;
-using NUnit.Framework;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -28,6 +27,8 @@ public class ComboManager : MonoBehaviour
      */
 
     private const float MAX_MULTIPLIER = 2f;
+    private const float SHAKE_DURATION = .8f;
+    private const float SHAKE_RANGE = 20f;
 
     private int _comboCount;
     private float _regenMultiplier;
@@ -90,14 +91,13 @@ public class ComboManager : MonoBehaviour
 
     private void UpdateComboCount(int newCount)
     {
-        Assert.GreaterOrEqual(newCount, 0);
         UpdateComboText(newCount);
 
         float newMultiplier = _regenMultiplier + newCount * PercentIncreasePerHit;
         _regenMultiplier = Mathf.Min(newMultiplier, MAX_MULTIPLIER);
 
         // DEBUGGING
-        Debug.Log("Regen Multiplier:\t" + _regenMultiplier);
+        // Debug.Log("Regen Multiplier:\t" + _regenMultiplier);
     }
 
     private void UpdateComboText(int newCount)
@@ -112,6 +112,27 @@ public class ComboManager : MonoBehaviour
             _comboText.enabled = true;
         }
 
-        // TODO: Play Animation
+        if (newCount == 1 || newCount % 10 == 0)
+            StartCoroutine(ShakeComboText());
+    }
+
+    private IEnumerator ShakeComboText()
+    {
+        float start = Time.time;
+        Quaternion origRot = _comboText.transform.rotation;
+        while (Time.time - start < SHAKE_DURATION)
+        {
+            // Shake Text
+            float z = Random.value * SHAKE_RANGE - SHAKE_RANGE / 2;
+            _comboText.transform.eulerAngles = new Vector3(origRot.x, origRot.y, origRot.z + z);
+
+            // Pingpong Color
+            _comboText.color = Color.Lerp(Color.black, Color.yellow, 
+                Mathf.PingPong(Time.time * 6f, 1f));
+            yield return new WaitForEndOfFrame();
+        }
+
+        _comboText.transform.rotation = origRot;
+        _comboText.color = Color.black;
     }
 }
